367edf1574751fb691e0240ce290dfda504ca4fd
Marek Loose <loose.marek@gmail.com>
2022-02-20 12:44:41 +0100

    Init example project

---
+++ SwiftPackageTutorial/SwiftPackageTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPackageTutorialApp.swift
+//  SwiftPackageTutorial
+//
+//  Created by Marek Loose on 20/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPackageTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftPackageTutorialUITests/SwiftPackageTutorialUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftPackageTutorialUITestsLaunchTests.swift
+//  SwiftPackageTutorialUITests
+//
+//  Created by Marek Loose on 20/02/2022.
+//
+
+import XCTest
+
+class SwiftPackageTutorialUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
